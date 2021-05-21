bc68619b1ed39da2bc7d04daefcdd0d5431f33d5
Lemon <lemon@git.com>
2021-05-22 00:48:22 +0800

    fist add

---
+++ src/main/java/com/markerhub/config/MybatisPlusConfig.java
@@ -0,0 +1,26 @@
+package com.markerhub.config;
+
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/**
+ * Created with IntelliJ IDEA
+ * Description：mybatis插件
+ * Author：Mr.LemonX
+ * Create：2021/5/21 22:56
+ * Company: Geek Studio
+ */
+@Configuration
+@EnableTransactionManagement
+@MapperScan("com.markerhub.mapper")
+public class MybatisPlusConfig {
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        PaginationInterceptor paginationInterceptor = new PaginationInterceptor();
+        return paginationInterceptor;
+    }
+
+}
---
+++ src/main/java/com/markerhub/controller/UserController.java
@@ -0,0 +1,29 @@
+package com.markerhub.controller;
+
+
+import com.markerhub.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ *  前端控制器
+ * </p>
+ *
+ * @author 关注公众号：MarkerHub
+ * @since 2021-05-21
+ */
+@RestController
+@RequestMapping("/user")
+public class UserController {
+    @Autowired
+    UserService userService;
+
+    @GetMapping("/index")
+    public Object test(){
+        return userService.getById(1L);
+    }
+}
